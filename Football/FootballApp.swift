d7e346a7cf35c93eadc43172a24ab83f253bda1f
Chris Bahn <csbahn@gmail.com>
2025-03-20 21:22:02 +0100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Chris Bahn on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
